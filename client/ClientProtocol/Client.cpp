54b101487b1dbdeaf2e04978668b87f46de50c91
winsbu1997 <38660429+winsbu1997@users.noreply.github.com>
2019-11-22 07:51:16 +0700

    Update Client.cpp
    finish

--- client/ClientProtocol/Client.cpp
+++ client/ClientProtocol/Client.cpp
@@ -3,41 +3,6 @@ struct addrinfo *result = NULL,
 	*ptr = NULL,
 	hints;
 
-u_short in_cksum(u_short *addr, int len)
-{
-	register int nleft = len;
-	register u_short *w = addr;
-	register u_short answer;
-	register int sum = 0;
-
-	/*
-	 *  Our algorithm is simple, using a 32 bit accumulator (sum),
-	 *  we add sequential 16 bit words to it, and at the end, fold
-	 *  back all the carry bits from the top 16 bits into the lower
-	 *  16 bits.
-	 */
-	while (nleft > 1) {
-		sum += *w++;
-		nleft -= 2;
-	}
-
-	/* mop up an odd byte, if necessary */
-	if (nleft == 1) {
-		u_short	u = 0;
-
-		*(u_char *)(&u) = *(u_char *)w;
-		sum += u;
-	}
-
-	/*
-	 * add back carry outs from top 16 bits to low 16 bits
-	 */
-	sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
-	sum += (sum >> 16);			/* add carry */
-	answer = ~sum;				/* truncate to 16 bits */
-
-	return (answer);
-}
 int TCP1(int ConnectSocket) {
 	int iResult;
 	char sendbuf[] = "Hello! I am TCP client!";
